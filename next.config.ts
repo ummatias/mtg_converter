45ef1536559f9dd53e75109e7d712770423e2170
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2025-04-30 16:36:49 -0300

    prepare for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/mtg_converter',
+  assetPrefix: '/mtg_converter',
+  trailingSlash: true, // important for GitHub Pages routing
 };
 
 module.exports = nextConfig;
