60fc57eb7fad36f8bfeb21e7536b069ed3b2b3a2
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2025-04-30 16:49:48 -0300

    fix: deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/mtg_converter',
+  assetPrefix: '/mtg_converter/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // importante para rotas estáticas no GitHub Pages
 };
 
 module.exports = nextConfig;
